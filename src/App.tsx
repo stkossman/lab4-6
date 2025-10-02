60f06e533ebfa2a16a1b116b1eacf16326d0bf68
Andrii Stavskyi <an.stawski@outlook.com>
2025-10-03 02:25:49 +0300

    refactor: replace Footer with Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import HomePage from "./pages/HomePage";
-import Footer from "./components/Footer";
-import ThemeToggle from "./components/ThemeToggle";
+import Header from "./components/Header";
 
 const App: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen font-light font-poppins bg-[#f5f5f5] dark:bg-neutral-900 transition-colors duration-300">
-      <header className="flex justify-end p-4">
-        <ThemeToggle />
-      </header>
+      <Header />
       <main className="flex-grow mb-8 px-4">
         <HomePage />
       </main>
-      <Footer />
     </div>
   );
 };
